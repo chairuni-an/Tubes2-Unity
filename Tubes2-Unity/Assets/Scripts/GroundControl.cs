3e8f10f27ae124447227bc647c4dbbbb12bc51f1
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-04-11 12:32:29 +0700

    Ethan can move, the ground move actually

---
+++ Tubes2-Unity/Assets/Scripts/GroundControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundControl : MonoBehaviour {
+
+	public float scrollSpeed = 0.5F;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		//rend.material.mainTextureOffset = new Vector2(offset, 0);
+		rend.material.mainTextureOffset = new Vector2(0, -offset);
+	}
+
+}
