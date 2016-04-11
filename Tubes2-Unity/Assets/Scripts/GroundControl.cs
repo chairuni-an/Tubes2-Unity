a74bdfdff19840fcc4444f43f3d0565da07d4aa8
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-04-11 14:27:30 +0700

    ethan walks better

--- Tubes2-Unity/Assets/Scripts/GroundControl.cs
+++ Tubes2-Unity/Assets/Scripts/GroundControl.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class GroundControl : MonoBehaviour {
 
-	public float scrollSpeed = 0.5F;
-	public Renderer rend;
+	float scrollSpeed = 0.5F;
+	Renderer rend;
 	void Start() {
 		rend = GetComponent<Renderer>();
 	}
 	void Update() {
 		float offset = Time.time * scrollSpeed;
-		//rend.material.mainTextureOffset = new Vector2(offset, 0);
 		rend.material.mainTextureOffset = new Vector2(0, -offset);
 	}
 
